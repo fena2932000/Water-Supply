aa7b8fa06b6bd66ca404b550f14187999f7e1831
nita998 <71010646+nita998@users.noreply.github.com>
2020-09-11 23:33:47 -0700

    mainactivity

--- app/src/main/java/com/example/watersupply/MainActivity.java
+++ app/src/main/java/com/example/watersupply/MainActivity.java
@@ -3,13 +3,17 @@ package com.example.watersupply;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
-Button btn;
+    TextView txt;
+ Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
