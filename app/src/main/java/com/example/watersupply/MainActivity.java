4886e252f55f0d569ee16e5dbee65a4d9714a6dc
fena2932000 <71011859+fena2932000@users.noreply.github.com>
2020-09-12 12:26:08 +0530

    btn1

--- app/src/main/java/com/example/watersupply/MainActivity.java
+++ app/src/main/java/com/example/watersupply/MainActivity.java
@@ -8,7 +8,7 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     TextView txt;
- Button btn;
+ Button btn1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
